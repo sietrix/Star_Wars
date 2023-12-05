aefd061c59c4608aebf447ec66864a32c112a80e
sietrix <823149+sietrix@users.noreply.github.com>
2023-12-05 13:26:07 +0100

    Montar escena con nave y luna, darle movimiento a la nave
    Montamos la escena con lo mínimo: un skybox, una luna y nuestra nave espacial. Después creamos el movimiento de la nave y su giro

---
+++ Assets/Models/Scripts/Player/Xwing.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Xwing : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField]
+    private int speed;
+    [SerializeField]
+    private int turnSpeed;
+
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        Movement();
+        Turning();
+    }
+
+    // Para mover mi nave espacial en el eje X y Z
+    void Movement()
+    {
+        float horizontal = Input.GetAxis("Horizontal"); // Teclas A y D
+        float vertical = Input.GetAxis("Vertical"); // Teclas W y D
+        // se le asigna los ejes (X, Y, Z)
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        transform.Translate(direction.normalized * speed * Time.deltaTime);
+    }
+
+    // Movemos nuestra nave en el eje Y, para girarla
+    void Turning()
+    {
+        float xMouse = Input.GetAxis("Mouse X"); // Recoger el desplazamiento horizontal del ratón
+        float yMouse = Input.GetAxis("Mouse Y"); // Recoger el desplazamiento vertical del ratón
+        Vector3 rotation = new Vector3(-yMouse, xMouse, 0);
+        transform.Rotate(rotation.normalized * turnSpeed * Time.deltaTime); // se normaliza el vector para que se mueva a la misma velocidad en todas las direcciones 
+    }
+
+
+}
